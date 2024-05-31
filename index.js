62836e47d891a1f6e261317699ecb385231c6cb9
md-maheen-billah <md.maheen.billah@icloud.com>
2024-05-31 23:41:14 +0200

    created an api which saves user information when logged in for first time

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middlewares
 app.use(express.json());
@@ -15,6 +16,70 @@ app.use(
     optionsSuccessStatus: 200,
   })
 );
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xxkyfyl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const db = client.db("destinedAffinityDB");
+    const usersCollection = db.collection("users");
+
+    // save a user data in db
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+
+      const query = { email: user?.email };
+      // check if user already exists in db
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        if (user.status === "requested") {
+          // if existing user try to change his role
+          const result = await usersCollection.updateOne(query, {
+            $set: { status: user?.status },
+          });
+          return res.send(result);
+        } else {
+          // if existing user login again
+          return res.send(isExist);
+        }
+      }
+
+      // save user for the first time
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now(),
+        },
+      };
+      const result = await usersCollection.updateOne(query, updateDoc, options);
+      // welcome new user
+      //   sendEmail(user?.email, {
+      //     subject: "Welcome to Stayvista!",
+      //     message: `Hope you will find you destination`,
+      //   });
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Destined Affinity Server is Running");
