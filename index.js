03ee28439164c3400966bee73f7d4e01cda6b9af
md-maheen-billah <md.maheen.billah@icloud.com>
2024-06-02 22:08:33 +0200

    created an api which can get requested data based on email

--- index.js
+++ index.js
@@ -232,6 +232,16 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/requested-access-dashboard/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await accessRequestCollection
+        .find({
+          email,
+        })
+        .toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
